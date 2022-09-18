8fefcaa0a91087dcd3dd86884e63d5290915d11f
Varga József Márk <vmark2145@gmail.com>
2022-09-18 20:05:30 +0200

    Drag események hozzáadva és implementálva

--- doc/src/js/App.js
+++ doc/src/js/App.js
@@ -15,3 +15,57 @@ var hungarianWords = [
 
 
 var empties = document.querySelectorAll('.empty');
+
+for (const empty of empties) {
+    empty.addEventListener("dragover", dragOver);
+    empty.addEventListener("dragenter", dragEnter);
+    empty.addEventListener('drop', dragDrop);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+}
+
+var englishWordsToInsert = getEnglishWords();
+var hungarianWordsToInsert = getHungarianWords();
+for (var i = 0; i < 4; i++) {
+    englishWords[i].addEventListener('dragstart', dragStart);
+    englishWords[i].addEventListener('dragend', dragEnd);
+    englishWords[i].setAttribute("data-english-word", englishWordsToInsert[i]);
+
+    hungarianWords[i].addEventListener('dragstart', dragStart);
+    hungarianWords[i].addEventListener('dragend', dragEnd);
+    hungarianWords[i].setAttribute("data-hungarian-word", hungarianWordsToInsert[i]);
+}
+
+function dragEnd() {
+    var element = document.getElementById(currentWordClassName + "Id");
+    element.className = currentWordClassName;
+}
+
+function dragStart() {
+    currentWordClassName = this.className
+    setTimeout(() => (this.className = 'invisible'), 0);
+    console.log("Drag started on element: " + currentWordClassName)
+}
+
+function dragDrop() {
+    var elementToAdd = document.getElementById(currentWordClassName + "Id");
+    this.append(elementToAdd);
+    this.className = 'empty'
+}
+
+function dragLeave() {
+    this.className = 'empty';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function handleDoneClick() {
+    alert('Done pressed!');
+}
