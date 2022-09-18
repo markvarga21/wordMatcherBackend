7fec5ad5acdac889695371b3192d4249b596944e
Varga József Márk <vmark2145@gmail.com>
2022-09-18 20:04:03 +0200

    Query selectorok megírva

---
+++ doc/src/js/App.js
@@ -0,0 +1,17 @@
+var currentWordClassName = '';
+
+var englishWords = [
+    document.querySelector('.englishWord1'),
+    document.querySelector('.englishWord2'),
+    document.querySelector('.englishWord3'),
+    document.querySelector('.englishWord4')
+];
+var hungarianWords = [
+    document.querySelector('.hungarianWord1'),
+    document.querySelector('.hungarianWord2'),
+    document.querySelector('.hungarianWord3'),
+    document.querySelector('.hungarianWord4')
+];
+
+
+var empties = document.querySelectorAll('.empty');
