2abe527f2653f7d8ca179e699fad4508652aa97f
Gabszter <cseres.gabor8@gmail.com>
2022-09-20 23:33:58 +0200

    Behúzott angol szavak eltárolása

--- doc/src/js/App.js
+++ doc/src/js/App.js
@@ -69,3 +69,15 @@ function dragEnter(e) {
 function handleDoneClick() {
     alert('Done pressed!');
 }
+
+var eng_words = []
+var hung_words = []
+
+function eng_word_block() {
+    for (var i = 1; i < 5; i++) {
+        var parent = document.getElementById('englishWordHolder'+i+'Id');
+        var childs = parent.firstChild;
+        var eng_w = childs.getAttribute("data-english-word");
+        eng_words.push(eng_w);
+    }
+}
