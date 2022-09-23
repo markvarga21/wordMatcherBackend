74cacba29a251506ce49f529d38d3faadb7c1fcb
Gabszter <cseres.gabor8@gmail.com>
2022-09-23 10:31:31 +0200

    validator átírása

--- doc/src/js/App.js
+++ doc/src/js/App.js
@@ -108,17 +108,23 @@ function fillDict(eng, hun) {
 
 function validator() {
     score = 0;
-    var eng = getEnglishWords();
-    var hun = getHungarianWords();
+    if (eng_words.length < 4 || hung_words.length < 4) {
+        return "Vannak még nem helyrerakott csempék, kérlek előbb rakd a helyére az összeset!"
+    }
+    var eng = getUnshuffledEnglishWords();
+    var hun = getUnshuffledHungarianWords();
+
+    var dictionary = fillDict(eng, hun)
+
     for (var i = 0; i < 4; i++) {
-        for (var k = 0; k < 4; k++){
-            if (eng[i] == eng_words[k]) {
-                if (hun[i] == hung_words[k])
-                score++;
-            }
+        console.log("Eng: " + eng_words[i] + ", hun: " + hung_words[i])
+        const englishWordToCheck = eng_words[i]
+        const hungarianWordToCheck = hung_words[i]
+        if (dictionary[englishWordToCheck] == hungarianWordToCheck) {
+            score++
         }
     }
-    return score;
+    return "Szerzett pontszám: " + score;
 }
 
 var save_english_word = "";
