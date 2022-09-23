3fe67123e5add7ed78f5d5449e426559661c4743
Fradi575 <78791893+Fradi575@users.noreply.github.com>
2022-09-23 21:44:35 +0200

    Függvények hozzáadása.
    Függvények hozzáadása.

--- doc/src/backend/service.py
+++ doc/src/backend/service.py
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
+import repository
+import random
+
+def getHungarianWords():
+    paired_words_all = repository.getHungarianWords()
+    randomed_four = dict()
+    for i in range(1, 5):
+        randomed_tuple = random.choice(list(paired_words_all.items()))
+        en = randomed_tuple[0]
+        hun = randomed_tuple[1]
+        randomed_four[en] = hun
+    return randomed_four
+
+def alreadyExistingPair(hungarian, english):
+    if repository.hungarianWordInUse(hungarian) or repository.englishWordInUse(english):
+        return True
+    else:
+        return False
+
+def obsceneWord(hungarian, english):
+    if repository.isObscene(hungarian) or repository.isObscene(english):
+        return True
+
+def saveWords(magyar, angol):
+    repository.saveWords(magyar, angol)
+
+
+=======
 import repository
 import random
 
@@ -12,3 +41,4 @@ def getHungarianWords():
     return randomed_four
 
 
+>>>>>>> c622b0d3ee7d8352cbfaa812af6528d9dcdd3f0f
