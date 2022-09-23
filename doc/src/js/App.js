5b545f9acabade8d241028cc4402ed237d3ee711
Gabszter <cseres.gabor8@gmail.com>
2022-09-23 10:29:57 +0200

    eng_word_block javítása

--- doc/src/js/App.js
+++ doc/src/js/App.js
@@ -80,8 +80,10 @@ function eng_word_block() {
     for (var i = 1; i < 5; i++) {
         var parent = document.getElementById('englishWordHolder'+i+'Id');
         var childs = parent.firstChild;
-        var eng_w = childs.getAttribute("data-english-word");
-        eng_words.push(eng_w);
+        if (childs != null) { 
+            var eng_w = childs.getAttribute("data-english-word");
+            eng_words.push(eng_w);
+        }
     }
 }
 
