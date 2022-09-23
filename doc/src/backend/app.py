e3f370487ab557f4399d5ef128c162e0e669a753
András Bódi <bodibandi113@gmail.com>
2022-09-23 21:30:35 +0200

    route hozzáadása

--- doc/src/backend/app.py
+++ doc/src/backend/app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-
 @app.route('/pairedWords', methods=['GET'])
 @cross_origin()
 def getMagyarSzavak():
@@ -17,16 +15,3 @@ def getMagyarSzavak():
 @app.route('/addWord', methods=['POST'])
 @cross_origin()
 def addWord():
-    magyar_szo = request.json['magyar_szo']
-    angol_szo = request.json['angol_szo']
-    
-    if service.alreadyExistingPair(magyar_szo, angol_szo):
-        return f'A {magyar_szo} es a {angol_szo} mar szerepelnek az adatbazisban!'
-
-    service.saveWords(magyar_szo, angol_szo)
-
-    print(magyar_szo, angol_szo)
-    return f"A {magyar_szo} es a {angol_szo} szavak hozza lettek adva az adatbazishoz!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
