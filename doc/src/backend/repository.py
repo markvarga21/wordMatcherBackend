9fd1c9303a6b790f4a504f96b7008c484c933f72
Fradi575 <78791893+Fradi575@users.noreply.github.com>
2022-09-23 21:50:43 +0200

    Függvények hozzáadása.

--- doc/src/backend/repository.py
+++ doc/src/backend/repository.py
@@ -6,3 +6,16 @@ redis_username = 'default'
 redis_password = 'WKgodsiY9vEE5aYlfIv8VuVu5VKnbgQr'
 
 rf = redis.Redis(host=redis_host, port=redis_port, username=redis_username, password=redis_password, decode_responses=True)
+
+def hungarianWordInUse(hungarian):
+    hu_ls = rf.lrange('HUNGARIA_WORDS_LIST', 0, -1)
+    return hungarian in hu_ls
+
+def englishWordInUse(english):
+    en_ls = rf.lrange('ENGLISH_WORDS_LIST', 0, -1)
+    return english in en_ls
+
+def isObscene(word):
+    obscene_ls = rf.lrange('FORBIDDEN_OBSCENE_WORDS', 0, -1)
+    return word in obscene_ls
+
