82af6196c0b972f8307ff09ef259db3b4626b860
András Bódi <bodibandi113@gmail.com>
2022-09-24 10:53:22 +0200

    Angol szavak bekérése kész

--- doc/src/js/WordProvider.js
+++ doc/src/js/WordProvider.js
@@ -7,3 +7,12 @@ function getUnshuffledEnglishWords() {
 function getUnshuffledHungarianWords() {
     return Object.values(words);
 }
+
+function getEnglishWords() {
+    console.log("Unshuffled english words: " + Object.keys(words))
+    let shuffled = Object.keys(words).sort(function () {
+        return Math.random() - 0.5;
+    })
+    console.log("Shuffled english words: " + shuffled)
+    return shuffled
+}
