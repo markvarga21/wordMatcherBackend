5557df518db98fa924f44f92c8ab4549d34e4219
András Bódi <bodibandi113@gmail.com>
2022-09-24 10:48:18 +0200

    Létező magyar angol szavak ellenőrzése a repóban kész

--- doc/src/backend/repository.py
+++ doc/src/backend/repository.py
@@ -6,3 +6,11 @@ redis_username = 'default'
 redis_password = 'WKgodsiY9vEE5aYlfIv8VuVu5VKnbgQr'
 
 rf = redis.Redis(host=redis_host, port=redis_port, username=redis_username, password=redis_password, decode_responses=True)
+
+def hungarianWordInUse(hungarian):
+    hu_ls = rf.lrange('HUNGARIA_WORDS_LIST', 0, -1)
+    return hungarian in hu_ls
+
+def englishWordInUse(english):
+    en_ls = rf.lrange('ENGLISH_WORDS_LIST', 0, -1)
+    return english in en_ls
