a585ab21ec8305ec859a411eb5f6efc43c46bd76
Gabszter <cseres.gabor8@gmail.com>
2022-09-24 14:32:44 +0200

    Save_Words.js létrehozása

--- doc/src/js/Hun_Eng_Word_Collector.js
+++ doc/src/js/Hun_Eng_Word_Collector.js
@@ -2,6 +2,7 @@ var eng_words = []
 var hung_words = []
 
 function eng_word_block() {
+    eng_words = [];
     for (var i = 1; i < 5; i++) {
         var parent = document.getElementById('englishWordHolder'+i+'Id');
         var childs = parent.firstChild;
@@ -13,6 +14,7 @@ function eng_word_block() {
 }
 
 function hung_word_block() {
+    hung_words = [];
     for (var i = 1; i < 5; i++) {
         var parent = document.getElementById('hungarianWordHolder'+i+'Id');
         var childs = parent.firstChild;
