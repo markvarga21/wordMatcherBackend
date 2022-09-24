7ddd74aeda182d47d1142ae9df4037fd76655290
Gabszter <cseres.gabor8@gmail.com>
2022-09-24 14:39:49 +0200

    validator törlése az App.js-ből

--- doc/src/js/App.js
+++ doc/src/js/App.js
@@ -87,27 +87,6 @@ function fillDict(eng, hun) {
     return dict
 }
 
-function validator() {
-    score = 0;
-    if (eng_words.length < 4 || hung_words.length < 4) {
-        return "Vannak még nem helyrerakott csempék, kérlek előbb rakd a helyére az összeset!"
-    }
-    var eng = getUnshuffledEnglishWords();
-    var hun = getUnshuffledHungarianWords();
-
-    var dictionary = fillDict(eng, hun)
-
-    for (var i = 0; i < 4; i++) {
-        console.log("Eng: " + eng_words[i] + ", hun: " + hung_words[i])
-        const englishWordToCheck = eng_words[i]
-        const hungarianWordToCheck = hung_words[i]
-        if (dictionary[englishWordToCheck] == hungarianWordToCheck) {
-            score++
-        }
-    }
-    return "Szerzett pontszám: " + score;
-}
-
 function checkAlpha(eng, hun) {
     for (let i = 0; i < eng.length; i++) {
         const c = eng[i]
