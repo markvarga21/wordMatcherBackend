364548c5a45468f3d68ef91fed12bc7277be6985
Gabszter <cseres.gabor8@gmail.com>
2022-09-24 14:28:16 +0200

    eng_word_block törlése az App.js-ből

--- doc/src/js/App.js
+++ doc/src/js/App.js
@@ -79,20 +79,6 @@ function handleDoneClick() {
     alert(sc);
 }
 
-var eng_words = []
-var hung_words = []
-
-function eng_word_block() {
-    for (var i = 1; i < 5; i++) {
-        var parent = document.getElementById('englishWordHolder'+i+'Id');
-        var childs = parent.firstChild;
-        if (childs != null) { 
-            var eng_w = childs.getAttribute("data-english-word");
-            eng_words.push(eng_w);
-        }
-    }
-}
-
 function hung_word_block() {
     for (var i = 1; i < 5; i++) {
         var parent = document.getElementById('hungarianWordHolder'+i+'Id');
